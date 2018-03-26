6e5300877c152c87e191696dd6b19b65879dd16d
Jackbob <jakobbertlin@outlook.com>
2018-03-26 15:45:45 +0200

    repo start and first funcs

---
+++ cmake-build-debug/CMakeFiles/MoA.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Decimation/DecimationMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Decimation/DecimationMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Decimation/QuadricDecimationMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Decimation/QuadricDecimationMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Decimation/SimpleDecimationMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Decimation/SimpleDecimationMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Fluid/FluidSolver.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Fluid/FluidSolver.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/FluidVoxelCutPlane.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/FluidVoxelCutPlane.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/FrameMain.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/FrameMain.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/GLAxis.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/GLAxis.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/GLGridPlane.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/GLGridPlane.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/GLObject.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/GLObject.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/GLViewer.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/GLViewer.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/GUI.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/GUI.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/ScalarCutPlane.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/ScalarCutPlane.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/GUI/VectorCutPlane.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/GUI/VectorCutPlane.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/Cube.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/Cube.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/HalfEdgeMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/HalfEdgeMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/Implicit.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/Implicit.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/ImplicitMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/ImplicitMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/LineStrip.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/LineStrip.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/Mesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/Mesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/Quadric.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/Quadric.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/SignedDistanceSphere.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/SignedDistanceSphere.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/SimpleMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/SimpleMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/Sphere.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/Sphere.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Geometry/SphereFractal.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Geometry/SphereFractal.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Levelset/LevelSet.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Levelset/LevelSet.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Levelset/LevelSetGrid.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Levelset/LevelSetGrid.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Levelset/LevelSetOperator.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Levelset/LevelSetOperator.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Math/TrilinearInterpolator.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Math/TrilinearInterpolator.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Subdivision/AdaptiveLoopSubdivisionMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Subdivision/AdaptiveLoopSubdivisionMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Subdivision/LoopSubdivisionMesh.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Subdivision/LoopSubdivisionMesh.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Subdivision/UniformCubicSpline.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Subdivision/UniformCubicSpline.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Subdivision/UniformCubicSplineSubdivisionCurve.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Subdivision/UniformCubicSplineSubdivisionCurve.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/BlackWhiteColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/BlackWhiteColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/ColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/ColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/ColorMapFactory.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/ColorMapFactory.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/GLObjectPlayback.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/GLObjectPlayback.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/GreenRedColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/GreenRedColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/HSVColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/HSVColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/Heap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/Heap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/HotColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/HotColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/IsoContourColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/IsoContourColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/JetColorMap.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/JetColorMap.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/MarchingCubes.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/MarchingCubes.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/ObjIO.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/ObjIO.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/Util.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/Util.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/Util/trackball.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/Util/trackball.cpp.o"
+  "/home/jakob/Documents/University/TNM079/tnm079-student/main.cpp" "/home/jakob/Documents/University/TNM079/tnm079-student/cmake-build-debug/CMakeFiles/MoA.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FREEGLUT_STATIC"
+  "LAB1"
+  "LAB2"
+  "LAB3"
+  "LAB4"
+  "LAB5"
+  "LAB6"
+  "WXUSINGDLL"
+  "_CRT_SECURE_NO_WARNINGS"
+  "_FILE_OFFSET_BITS=64"
+  "_SCL_SECURE_NO_WARNINGS"
+  "_USE_MATH_DEFINES"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu/wx/include/gtk2-unicode-3.0"
+  "/usr/include/wx-3.0"
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
